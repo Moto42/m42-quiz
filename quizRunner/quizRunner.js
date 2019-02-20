ab086f369074eeab1c6875dd85bae9f6c4a2cf6f
Moto42 <moto42@gmail.com>
2019-02-20 12:44:22 -0600

    rearanged to be easier to find, makes a little more sense.

--- quizRunner/quizRunner.js
+++ quizRunner/quizRunner.js
@@ -1,75 +1,6 @@
 
 const quizRunner = {};
 
-quizRunner.objectToArray    =       function (obj)          {
-  let arr = [];
-  for(let i in obj) {
-    arr.push(obj[i]);
-  }
-  return arr;
-};
-quizRunner.clickAnswer      =       function (e)            {
-  const results  = $(e.target).data('results') ? $(e.target).data('results').split(',') : [];
-  results.forEach((item) => {
-    quizRunner.quiz.results[item].score = quizRunner.quiz.results[item].score + 1;
-  });
-  quizRunner.nextQuestion();
-};
-quizRunner.getQuizID        =       function ()             {
-  return window.location.search.split('?')[1];
-};
-quizRunner.setDocTitle      =       function (title)        {
-  document.title = title;
-};
-quizRunner.setTitle         =       function (title)        {
-  $('#quizTitle').text(title);
-};
-quizRunner.setBlurb         =       function (blurb)        {
-  $('#quizBlurb').text(blurb);
-};
-quizRunner.setQuestionText  =       function (question)     {
-  $('#questionText').text(question);
-};
-quizRunner.createAnswer     =       function (text,results) {
-  let answer = $(`<div>${text}</div>`)
-    .addClass('answer')
-    .attr('data-results',results)
-    .click(quizRunner.clickAnswer);
-  return answer;
-};
-quizRunner.populateAnswers  =       function (answers)      {
-  answers.forEach(answer => {
-    const element = quizRunner.createAnswer(answer.answer, answer.results.join(","));
-    $('#answers').append(element);
-  });
-};
-quizRunner.clearAnswers     =       function ()             {
-  $('#answers').children().remove();
-};
-quizRunner.displayQuestion  =       function (question)     {
-  quizRunner.setQuestionText(question.question);
-  quizRunner.clearAnswers();
-  quizRunner.populateAnswers(question.answers);
-};
-quizRunner.initializeScores =       function ()             {
-  for(let i in quizRunner.quiz.results){
-    quizRunner.quiz.results[i].score = 0;
-  }
-};
-quizRunner.nextQuestion     =       function ()             {
-  quizRunner.quiz.currentQuestion = quizRunner.quiz.currentQuestion + 1;
-  if (quizRunner.quiz.currentQuestion === quizRunner.quiz.questions.length) {
-    quizRunner.displayResults();
-    return;
-  }
-  else {
-    quizRunner.displayQuestion(quizRunner.quiz.questions[quizRunner.quiz.currentQuestion]);
-  }
-};
-quizRunner.loadQuiz         = async function (quizID)       {
-  const quiz = await fetch(`quizes/${quizID}.json`).then(res =>res.json());
-  return quiz;
-};
 quizRunner.populateQuiz     = async function ()             {
   const quizID = quizRunner.getQuizID();
   quizRunner.quiz = await quizRunner.loadQuiz(quizID);
@@ -81,9 +12,71 @@ quizRunner.populateQuiz     = async function ()             {
   quizRunner.displayQuestion(quizRunner.quiz.questions[quizRunner.quiz.currentQuestion]);
   quizRunner.startButtonOn();
 };
+quizRunner.getQuizID        =       function ()             {
+  return window.location.search.split('?')[1];
+};
+quizRunner.loadQuiz         = async function (quizID)       {
+  const quiz = await fetch(`quizes/${quizID}.json`).then(res =>res.json());
+  return quiz;
+};
+quizRunner.initializeScores =       function ()             {
+  for(let i in quizRunner.quiz.results){
+    quizRunner.quiz.results[i].score = 0;
+  }
+};
+quizRunner.objectToArray    =       function (obj)          {
+  let arr = [];
+  for(let i in obj) {
+    arr.push(obj[i]);
+  }
+  return arr;
+};
+quizRunner.setTitle         =       function (title)        {
+  $('#quizTitle').text(title);
+};
+quizRunner.setDocTitle      =       function (title)        {
+  document.title = title;
+};
+quizRunner.setBlurb         =       function (blurb)        {
+  $('#quizBlurb').text(blurb);
+};
+quizRunner.displayQuestion  =       function (question)     {
+  quizRunner.setQuestionText(question.question);
+  quizRunner.clearAnswers();
+  quizRunner.populateAnswers(question.answers);
+};
+quizRunner.nextQuestion     =       function ()             {
+  quizRunner.quiz.currentQuestion = quizRunner.quiz.currentQuestion + 1;
+  if (quizRunner.quiz.currentQuestion === quizRunner.quiz.questions.length) {
+    quizRunner.displayResults();
+    return;
+  }
+  else {
+    quizRunner.displayQuestion(quizRunner.quiz.questions[quizRunner.quiz.currentQuestion]);
+  }
+};
 quizRunner.startButtonOn    =       function ()             {
   $('#start').prop('disabled', false);
   $('#start').click(quizRunner.startQuiz);
+};
+quizRunner.setQuestionText  =       function (question)     {
+  $('#questionText').text(question);
+};
+quizRunner.clearAnswers     =       function ()             {
+  $('#answers').children().remove();
+};
+quizRunner.populateAnswers  =       function (answers)      {
+  answers.forEach(answer => {
+    const element = quizRunner.createAnswer(answer.answer, answer.results.join(","));
+    $('#answers').append(element);
+  });
+};
+quizRunner.createAnswer     =       function (text,results) {
+  let answer = $(`<div>${text}</div>`)
+    .addClass('answer')
+    .attr('data-results',results)
+    .click(quizRunner.clickAnswer);
+  return answer;
 };
 quizRunner.fillInResults    =       function (results)      {
   const array = quizRunner.objectToArray(results);
@@ -97,6 +90,13 @@ quizRunner.displayResults   =       function ()             {
   $('#question').addClass('hide');
   quizRunner.fillInResults(quizRunner.quiz.results);
   $('#result').removeClass('hide');
+};
+quizRunner.clickAnswer      =       function (e)            {
+  const results  = $(e.target).data('results') ? $(e.target).data('results').split(',') : [];
+  results.forEach((item) => {
+    quizRunner.quiz.results[item].score = quizRunner.quiz.results[item].score + 1;
+  });
+  quizRunner.nextQuestion();
 };
 quizRunner.startQuiz        =       function ()             {
   $("#introduction").addClass('hide');
