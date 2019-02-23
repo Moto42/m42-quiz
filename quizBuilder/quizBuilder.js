276c46b633340a0dc0caf806415ab4bf5df51c79
Moto42 <moto42@gmail.com>
2019-02-23 17:49:52 -0600

    remove result now remove itself from the answers lists.

--- quizBuilder/quizBuilder.js
+++ quizBuilder/quizBuilder.js
@@ -3,16 +3,18 @@ const quizBuilder = {};
 
 quizBuilder.resultsCount    = 0;
 
-quizBuilder.newResultNode   = (resultID)   => {
-  return (
+quizBuilder.newResultNode   = (resultID)     => {
+  let node = $(
     `<div class="result" data-resultID="${resultID}">
     <input type="text" class="rName">
     <button class="removeResult">X</button>
     <textarea name="" id="" cols="30" rows="10" class="rBlurb"></textarea>
     </div>`
   );
+  node.find('.removeResult').click(quizBuilder.removeResult);
+  return node;
 };
-quizBuilder.newQuestionNode = ()           => {
+quizBuilder.newQuestionNode = ()             => {
   let newNode = $(
     `<tr class="question">
     <td class="questionText">Test</td>
@@ -21,7 +23,7 @@ quizBuilder.newQuestionNode = ()           => {
   );
   return newNode;
 };
-quizBuilder.newVoteHeader   = (resultID)   => {
+quizBuilder.newVoteHeader   = (resultID)     => {
   return(`
     <td class="voteHeader" data-resultid=${resultID}>
       ${resultID}
@@ -29,17 +31,18 @@ quizBuilder.newVoteHeader   = (resultID)   => {
   `);
 };
 
-quizBuilder.inializeBuilder = ()           => {
+quizBuilder.inializeBuilder = ()             => {
   //add event to the #newResult button
   $('#newResult').click(quizBuilder.addNewResult);
   //add event to the #newQuestion button.
   $('#newQuestion').click(quizBuilder.addNewQuestion);
 };
-quizBuilder.addNewResult    = (e)          => {
+quizBuilder.addNewResult    = (e)            => {
   e.preventDefault();
   quizBuilder.resultsCount = quizBuilder.resultsCount + 1;
   const resultID = `resultid${quizBuilder.resultsCount}`;
-  $('#qResultsList').append(quizBuilder.newResultNode(resultID));
+  const resultNode = quizBuilder.newResultNode(resultID);
+  $('#qResultsList').append(resultNode);
   //Add result to the #qQuestionsHeader
   $('#qQuestionsHeader').append(quizBuilder.newVoteHeader(resultID));
   // Add new votbox to each question
@@ -47,7 +50,7 @@ quizBuilder.addNewResult    = (e)          => {
     quizBuilder.addVoteBox(resultID, $(e));
   });
 };
-quizBuilder.addNewQuestion  = (e)          => {
+quizBuilder.addNewQuestion  = (e)            => {
   e.preventDefault();
   const newNode = quizBuilder.newQuestionNode();
   $('#qQuestionsList').append(newNode);
@@ -64,5 +67,22 @@ quizBuilder.addVoteBox      = (id, question) => {
     </td>`
   ) );
 };
+quizBuilder.removeVoteBox   = (id, question) => {
+  $(question).find(`[data-resultid = ${id}]`).parent().remove();
+};
+quizBuilder.removeResult    = (e)       => {
+  e.preventDefault();
+  let result = $(e.target).parent();
+  let resultID = result.data('resultid');
+  result.remove();
+  $('.question').each((i,q) => quizBuilder.removeVoteBox(resultID, q));
+  $('#qQuestionsHeader').find(`[data-resultid = "${resultID}"]`).remove();
+};
+quizBuilder.removeQuestion  = (e)       => {
+  e.preventDefault();
+  console.log(e.target);
+  // TODO: quizBuilder.removeQuestion
+};
+
 
 if(typeof module !== 'undefined') module.exports = quizRunner;
